e24226f8f1080286709af53cb2924b4a3160ab4f
wangmengdan.wmd <wangmengdan.wmd@bytedance.com>
2023-10-29 22:36:31 +0800

    feat: 1.19.3

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,46 +1,35 @@
-import {
-  Await,
-  useFetcher,
-  useLoaderData,
-  useRevalidator,
-} from '@remix-run/react';
+import { Await, useFetcher, useLoaderData } from '@remix-run/react';
 import { Suspense, useMemo } from 'react';
 import { mainLoader } from '~/services/main.server';
 
 export const loader = mainLoader;
 export default function Index() {
-  const { list, list2 } = useLoaderData<typeof loader>();
-  const { revalidate } = useRevalidator();
-  const { submit, load } = useFetcher();
-  const promise = useMemo(() => Promise.all([list, list2]), [list, list2]);
-
-  console.log('render again');
+  const { isAuth, welfare, randomNum } = useLoaderData<typeof loader>();
+  const { submit } = useFetcher();
+  const promise = useMemo(
+    () => Promise.all([isAuth, welfare]),
+    [isAuth, welfare]
+  );
   return (
     <div>
-      <h1 onClick={() => load('/api/adjust')}>fetcher.load click</h1>
-      <h1
-        onClick={() => submit(null, { action: '/api/adjust', method: 'post' })}
-      >
-        fetcher.submit click
-      </h1>
-      <h1 onClick={revalidate}>revalidate click</h1>
-
-      <Suspense fallback={<div>fallback</div>}>
+      <h1>{randomNum}</h1>
+      <Suspense fallback={<div>loading....</div>}>
         <Await resolve={promise}>
-          {([list, list2]) => {
+          {([isAuth, welfare]) => {
             return (
-              <>
-                {list?.map((item) => (
-                  <div key={item.name} style={{ color: 'red' }}>
-                    {item?.name}
-                  </div>
-                ))}
-                {list2?.map((item) => (
-                  <div key={item.name} style={{ color: 'yellowgreen' }}>
-                    {item?.name}
-                  </div>
-                ))}
-              </>
+              <span
+                onClick={() => {
+                  if (isAuth && !welfare?.is_received) {
+                    submit(null, { action: '/api/receive', method: 'post' });
+                  }
+                }}
+              >
+                {!isAuth
+                  ? 'authorize'
+                  : welfare?.is_received
+                  ? 'received'
+                  : 'receive'}
+              </span>
             );
           }}
         </Await>
--- app/services/main.server.ts
+++ app/services/main.server.ts
@@ -1,32 +1,38 @@
-import type { ActionArgs, LoaderArgs} from "@remix-run/node";
-import { defer, json } from "@remix-run/node";
-
+import type { ActionArgs, LoaderArgs } from '@remix-run/node';
+import { defer, json } from '@remix-run/node';
 
 export const getList = async (index: string) => {
-  const list: Promise<{ name: string }[]> = new Promise((r) => {
+  const list = new Promise((r) => {
     setTimeout(() => {
-      r([
-        { name: `list${index} name1` },
-        { name: `list${index} name2` },
-      ]);
+      r([{ name: `list${index} name1` }, { name: `list${index} name2` }]);
     }, 1000);
   });
   return await list;
 };
 
-
-export const mainLoader = async (args: LoaderArgs) => {
-  const list = getList('1');
-  const list2 = getList('2');
-  const list3 = getList('3');
-  const list4 = getList('4');
-  return defer({ list: list, list2, list3, list4 });
+const getAuth = async (): Promise<boolean> => {
+  return await new Promise((r) => {
+    setTimeout(() => {
+      r(true);
+    }, 1000);
+  });
 };
 
+const getWelfare = async (): Promise<{ is_received: boolean }> => {
+  return await new Promise((r) => {
+    setTimeout(() => {
+      r({ is_received: false });
+    }, 1000);
+  });
+};
 
-
-
-
+export const mainLoader = async (args: LoaderArgs) => {
+  return defer({
+    isAuth: getAuth(),
+    welfare: getWelfare(),
+    randomNum: Math.random() * 100,
+  });
+};
 
 const getAdjustUrl = async () => {
   const aa: Promise<{ url: string }> = new Promise((r) => {
